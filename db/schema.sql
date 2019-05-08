8b0d725bd285247d69fa23645b1496f24eaa1df4
Darkangel <wtsegars@mac.com>
2019-05-08 16:36:23 -0400

    db/

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,16 @@ USE wh40k_db;
 
 CREATE TABLE armies(
 	id INT(3) AUTO_INCREMENT NOT NULL,
-    army_name VARCHAR(15) NOT NULL,
+    army_name VARCHAR(30) NOT NULL,
     play BOOLEAN, 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE figures(
 	id INT(3) AUTO_INCREMENT NOT NULL,
-    figure_name VARCHAR(25) NOT NULL,
+    figure_name VARCHAR(35) NOT NULL,
+    faction VARCHAR(30) NOT NULL,
+    role VARCHAR(15),
     own BOOLEAN,
     PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,141 @@
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties: Blood Angels", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties: Dark Angels", true);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties: Deathwatch", true);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties: Grey Knights", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Astarties: Space Wolves", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Custodes", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Astera Militarum", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adeptus Mechanicus", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Adepta Sororitas", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Imperial Knights", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Imperial Agents", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Chaos Daemons", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Heritic Astarties", true);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Heritic Astarties: Death Guard", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Heritic Astarties: Thousand Sons", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Renegade Knights", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Aeldari", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Drukhari", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Harlequins", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Genestealer Cults", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Tyranids", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Necrons", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("Orks", false);
+
+INSERT INTO armies (army_name, play)
+VALUES ("T'au Empire", true);
+
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Tactical Squad", "Adeptus Astarties: Dark Angels", "Troops", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Assault Squad", "Adeptus Astarties: Dark Angels", "Fast Attack", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Ravenwing Bike Squad", "Adeptus Astarties: Dark Angels", "Fast Attack", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Deathwing Terminator Squad", "Adeptus Astarties: Dark Angels", "Elites", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Hellblaster Squad", "Adeptus Astarties: Dark Angels", "Heavy Support", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Drop Pod", "Adeptus Astarties: Dark Angels", "Dedicated Transport", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Land Raider Redeemer", "Adeptus Astarties: Dark Angels", "Heavy Support", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Company Veteran Squad", "Adeptus Astarties: Dark Angels", "Elites", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Interogator Chaplian", "Adeptus Astarties: Dark Angels", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Company Master", "Adeptus Astarties: Dark Angels", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Librarian", "Adeptus Astarties: Dark Angels", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Pathfinder Squad", "T'au Empire", "Fast Attack", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Firewarrior Squad", "T'au Empire", "Troops", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("XV8 Crisis Suits", "T'au Empire", "Troops", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Etherial on Hover Drone", "T'au Empire", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("TX7 Hammerhead Gunship", "T'au Empire", "Heavy Support", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Kill Team", "Adeptus Astarties: Deathwatch", "Troops", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Watch Master", "Adeptus Astarties: Deathwatch", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Terminator Captian", "Adeptus Astarties: Deathwatch", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Venerable Dreadnaught", "Adeptus Astarties: Deathwatch", "Elites", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Watch Captian Artimis", "Adeptus Astarties: Deathwatch", "HQ", true);
+
+INSERT INTO figues (figure_name, faction, role, own)
+VALUES ("Assault Terminators", "Adeptus Astarties: Deathwatch", "Troops", true);
