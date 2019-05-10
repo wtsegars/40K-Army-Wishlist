4bc04f08681c0ec875750cec367a52d2c2c54260
Darkangel <wtsegars@mac.com>
2019-05-10 00:12:16 -0400

    config/

--- config/orm.js
+++ config/orm.js
@@ -3,25 +3,19 @@ const connection = require('./connection.js');
 let orm = {
     selectAll: function(tableSelect) {
         connection.query("SELECT * FROM ??", [tableSelect], function(err, result) {
-            if (err) {
-                return 
-            }
+            if (err) throw err;
             console.log(result);
         });
     },
     insertOne: function(newInfo, tableSelect) {
         connection.query("INSERT ?? INTO ??", [newInfo, tableSelect], function(err, result) {
-            if (err) {
-                return
-            }
+            if (err) throw err;
             console.log(result);
         });
     },
     updateOne: function(updatedInfo, tableSelect) {
         connection.query("UPDATE ?? WHERE ??", [updatedInfo, tableSelect], function(err, result) {
-            if (err) {
-                return
-            }
+            if (err) throw err;
             console.log(result);
         });
     }
