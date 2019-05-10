2ac7038053eb4d191b1c2bf7344b5bb4388ac948
Darkangel <wtsegars@mac.com>
2019-05-10 00:12:32 -0400

    controllers/

--- controllers/wh40k_contrillers.js
+++ controllers/wh40k_contrillers.js
@@ -5,7 +5,7 @@ const wh40k = require('../models/wh40k');
 const router = express.router();
 
 router.get("/", function(req, res) {
-    wh40k.all(function(data) {
+    wh40k.selectAll(function(data) {
         let hbsObject = {
             wh40k: data
         };
@@ -15,5 +15,29 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/wh40k", function(req, res) {
-    wh40k.create(["figure_name", "faction", "role", "own"], [req.body.figureName, req.body.faction, req.body.role, req.body.own])
-})
+    wh40k.insertOne(["figure_name", "faction", "role", "own"], [req.body.figureName, req.body.faction, req.body.role, req.body.own], function(data) {
+        res.json({ figure_name: data.insertName });
+    });
+});
+
+router.put("/api/wh40k/:name", function(req, res) {
+    let condition = "name = " + req.params.figure_name;
+
+    wh40k.updateOne(
+        {
+            figure_name: req.body.figure_name,
+            faction: req.body.faction,
+            role: req.body.role,
+            own: req.body.own
+        },
+        condition,
+        function(result) {
+            if(result === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
