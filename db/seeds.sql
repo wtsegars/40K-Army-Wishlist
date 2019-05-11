263ecb100cd3b3d2603811aa8bd83d2802a29305
Darkangel <wtsegars@mac.com>
2019-05-11 10:05:00 -0400

    db/

--- db/seeds.sql
+++ db/seeds.sql
@@ -74,68 +74,68 @@ INSERT INTO armies (army_name, play)
 VALUES ("T'au Empire", true);
 
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Tactical Squad", "Adeptus Astarties: Dark Angels", "Troops", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Assault Squad", "Adeptus Astarties: Dark Angels", "Fast Attack", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Ravenwing Bike Squad", "Adeptus Astarties: Dark Angels", "Fast Attack", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Deathwing Terminator Squad", "Adeptus Astarties: Dark Angels", "Elites", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Hellblaster Squad", "Adeptus Astarties: Dark Angels", "Heavy Support", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Drop Pod", "Adeptus Astarties: Dark Angels", "Dedicated Transport", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Land Raider Redeemer", "Adeptus Astarties: Dark Angels", "Heavy Support", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Company Veteran Squad", "Adeptus Astarties: Dark Angels", "Elites", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Interogator Chaplian", "Adeptus Astarties: Dark Angels", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Company Master", "Adeptus Astarties: Dark Angels", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Librarian", "Adeptus Astarties: Dark Angels", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Pathfinder Squad", "T'au Empire", "Fast Attack", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Firewarrior Squad", "T'au Empire", "Troops", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("XV8 Crisis Suits", "T'au Empire", "Troops", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Etherial on Hover Drone", "T'au Empire", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("TX7 Hammerhead Gunship", "T'au Empire", "Heavy Support", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Kill Team", "Adeptus Astarties: Deathwatch", "Troops", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Watch Master", "Adeptus Astarties: Deathwatch", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Terminator Captian", "Adeptus Astarties: Deathwatch", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Venerable Dreadnaught", "Adeptus Astarties: Deathwatch", "Elites", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Watch Captian Artimis", "Adeptus Astarties: Deathwatch", "HQ", true);
 
-INSERT INTO figues (figure_name, faction, role, own)
+INSERT INTO figures (figure_name, faction, role, own)
 VALUES ("Assault Terminators", "Adeptus Astarties: Deathwatch", "Troops", true);
