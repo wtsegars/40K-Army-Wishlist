53146f7a0f322bb29b91be966cd348628eb31557
Darkangel <wtsegars@mac.com>
2019-05-13 17:27:32 -0400

    public/

---
+++ public/assests/js/app.js
@@ -0,0 +1,39 @@
+S(function() {
+    $(".change-own").on("click", function(event) {
+        var id = $(this).data("id");
+        var newFigure = $(this).data("newFigure");
+
+        var ownership = {
+            own: newFigure
+        };
+
+        $.ajax("/api/wh40k" + id, {
+            type: "PUT",
+            data: ownership
+        }).then(function() {
+            console.log("Changed own to ", newFigure);
+
+            location.reload();
+        });
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newModel = {
+            figure_name: $("#ca").val().trim(),
+            faction: $("#fa").val().trim(),
+            role: $("#rl").val().trim(),
+            own: $("[name=own]:checked").val().trim()
+        };
+
+        $.ajax("/api/wh40k", {
+            type: "POST",
+            data: newModel
+        }).then(function() {
+            console.log("New Model Added");
+
+            location.reload();
+        });
+    });
+});
